f453b8e6e1d6745491aedea8ed3d84f4922e7d2a
Anton Aftakhov <simple1elf@gmail.com>
2025-09-07 23:11:09 +0500

    allure-playwright: 3.2.1

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ const config: PlaywrightTestConfig = {
       {
         environmentInfo: {
           'playwright': '1.52.0',
-          'allure-playwright': '3.2.0'
+          'allure-playwright': '3.2.1'
         },
         resultsDir: 'playwright-report',
       },
