e84fe7a434b85969be7ca59dd08b6b6d5717be89
Anton Aftakhov <simple1elf@gmail.com>
2025-09-10 20:21:04 +0500

    playwright: 1.54.0 + allure-playwright: 3.4.0

--- src/tests/example-diff.spec.ts
+++ src/tests/example-diff.spec.ts
@@ -1,24 +1,34 @@
-import { test, expect } from '@playwright/test';
-import * as allure from 'allure-js-commons';
+import {expect, test} from "@playwright/test";
+import * as allure from "allure-js-commons";
 
-test.beforeEach(async ({}, testInfo) => {
-  await allure.attachment('beforeEach-attachment', 'Данные перед тестом', 'text/plain');
+test.beforeAll(async ({}, testInfo) => {
+  await allure.attachment('beforeAll-attachment', 'Данные перед тестом All', 'text/plain');
 });
 
-test.afterEach(async ({}, testInfo) => {
-  await allure.attachment('afterEach-attachment', 'Данные после теста', 'text/plain');
+test.afterAll(async ({}, testInfo) => {
+  await allure.attachment('afterAll-attachment', 'Данные после теста All', 'text/plain');
 });
 
-test('Главная страница содержит заголовок diff', async ({ page }) => {
-  await allure.attachment('test-attachment', 'Данные внутри теста', 'text/plain');
-
-  await allure.step('Переход на главную страницу', async () => {
-    await page.goto('/');
-    await allure.attachment('step-attachment', 'Данные внутри шага', 'text/plain');
+test.describe("Тест класс", async () => {
+  test.beforeEach(async ({}, testInfo) => {
+    await allure.attachment('beforeEach-attachment', 'Данные перед тестом', 'text/plain');
   });
 
-  await allure.step('Проверка заголовка', async () => {
-    await expect(page).toHaveTitle(/Playwright/);
-    await allure.attachment('title-step-attachment', 'Заголовок проверен', 'text/plain');
+  test.afterEach(async ({}, testInfo) => {
+    await allure.attachment('afterEach-attachment', 'Данные после теста', 'text/plain');
   });
-});
+
+  test('Главная страница содержит заголовок', async ({ page }) => {
+    await allure.attachment('test-attachment', 'Данные внутри теста', 'text/plain');
+
+    await allure.step('Переход на главную страницу', async () => {
+      await page.goto('/');
+      await allure.attachment('step-attachment', 'Данные внутри шага', 'text/plain');
+    });
+
+    await allure.step('Проверка заголовка', async () => {
+      await expect(page).toHaveTitle(/Playwright/);
+      await allure.attachment('title-step-attachment', 'Заголовок проверен', 'text/plain');
+    });
+  });
+})
--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,24 +1,34 @@
 import { test, expect } from '@playwright/test';
 import * as allure from 'allure-js-commons';
 
-test.beforeEach(async ({}, testInfo) => {
-  await allure.attachment('attachment', 'Данные перед тестом', 'text/plain');
+test.beforeAll(async ({}, testInfo) => {
+  await allure.attachment('attachment', 'Данные перед тестом All', 'text/plain');
 });
 
-test.afterEach(async ({}, testInfo) => {
-  await allure.attachment('attachment', 'Данные после теста', 'text/plain');
+test.afterAll(async ({}, testInfo) => {
+  await allure.attachment('attachment', 'Данные после теста All', 'text/plain');
 });
 
-test('Главная страница содержит заголовок', async ({ page }) => {
-  await allure.attachment('attachment', 'Данные внутри теста', 'text/plain');
-
-  await allure.step('Переход на главную страницу', async () => {
-    await page.goto('/');
-    await allure.attachment('attachment', 'Данные внутри шага', 'text/plain');
+test.describe("Тест класс", async () => {
+  test.beforeEach(async ({}, testInfo) => {
+    await allure.attachment('attachment', 'Данные перед тестом', 'text/plain');
   });
 
-  await allure.step('Проверка заголовка', async () => {
-    await expect(page).toHaveTitle(/Playwright/);
-    await allure.attachment('attachment', 'Заголовок проверен', 'text/plain');
+  test.afterEach(async ({}, testInfo) => {
+    await allure.attachment('attachment', 'Данные после теста', 'text/plain');
   });
-});
+
+  test('Главная страница содержит заголовок', async ({ page }) => {
+    await allure.attachment('attachment', 'Данные внутри теста', 'text/plain');
+
+    await allure.step('Переход на главную страницу', async () => {
+      await page.goto('/');
+      await allure.attachment('attachment', 'Данные внутри шага', 'text/plain');
+    });
+
+    await allure.step('Проверка заголовка', async () => {
+      await expect(page).toHaveTitle(/Playwright/);
+      await allure.attachment('attachment', 'Заголовок проверен', 'text/plain');
+    });
+  });
+})
